17b0d98ccd9094bf319c632b6c3db38a0018f5f6
AtharvaM-1732 <122292578+AtharvaM-1732@users.noreply.github.com>
2024-05-20 20:51:54 +0530

    Update 2. Merge-Sort.cpp

--- 2. Merge-Sort.cpp
+++ 2. Merge-Sort.cpp
@@ -1,77 +1,69 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void merge(int arr1[], int arr2[], int merged[], int size1, int size2);
+#define MAX 99
+
+int merge_R(int a[], int low, int up);
+int merge(int a[], int temp[], int low1, int up1, int low2, int up2);
+int copy(int a[], int temp[], int low, int up);
 
 int main() {
-    int size1, size2;
-    cout << "Enter the size of the first array: ";
-    cin >> size1;
-    int arr1[size1];
-    cout << "Enter elements of the first array:\n";
-    for (int i = 0; i < size1; i++) {
-        cin >> arr1[i];
-    }
+   int a[]={1,10,5,7,6};
+   
+   merge_R(a,0,4);
 
-    cout << "Enter the size of the second array: ";
-    cin >> size2;
-    int arr2[size2];
-    cout << "Enter elements of the second array:\n";
-    for (int i = 0; i < size2; i++) {
-        cin >> arr2[i];
-    }
-
-    int merged[size1 + size2];
-    merge(arr1, arr2, merged, size1, size2);
-
-    cout << "\nThe merged and sorted array is:\n";
-    for (int i = 0; i < size1 + size2; i++) {
-        cout << merged[i] << " ";
-    }
-    cout << endl;
-
+   for(int i=0; i<5; i++)
+   {
+       cout<<a[i]<<" ";
+   }
     return 0;
 }
 
-void merge(int arr1[], int arr2[], int merged[], int size1, int size2) {
-    int i = 0, j = 0, k = 0;
-
-    while (i < size1 && j < size2) {
-        if (arr1[i] <= arr2[j]) {
-            merged[k++] = arr1[i++];
-        } else {
-            merged[k++] = arr2[j++];
-        }
-    }
-
-    while (i < size1) {
-        merged[k++] = arr1[i++];
-    }
-
-    while (j < size2) {
-        merged[k++] = arr2[j++];
-    }
-
-    // Sort the merged array
-    for (int m = 0; m < size1 + size2 - 1; m++) {
-        for (int n = 0; n < size1 + size2 - m - 1; n++) {
-            if (merged[n] > merged[n + 1]) {
-                int temp = merged[n];
-                merged[n] = merged[n + 1];
-                merged[n + 1] = temp;
-            }
-        }
+int merge_R(int a[], int low, int up)
+{
+    int temp[MAX];
+    int mid=(low+up)/2;
+    if(low<up){
+        int mid=(low+up)/2;
+    merge_R(a,low, mid);
+    merge_R(a,mid+1,up);
+    merge(a,temp,low,mid,mid+1,up);
+    copy(a,temp,low,up);
     }
 }
-//Output
-/*
-Enter the size of the first array: 2
-Enter elements of the first array:
-22 42
-Enter the size of the second array: 3
-Enter elements of the second array:
-44 12 18 1
 
-The merged and sorted array is:
-12 18 22 42 44
-*/
+int merge(int a[], int temp[], int low1, int up1, int low2, int up2)
+{
+    int i=low1;
+    int j=low2;
+    int k=low1;
+    while(i<=up1 && j<=up2)
+        {
+            if(a[i]<=a[j])
+            {
+                temp[k++]=a[i++];
+            }
+            else
+            {
+                temp[k++]=a[j++];
+            }
+            
+        }    
+    while(i<=up1)
+    {
+        temp[k++]=a[i++];
+    }
+    while(j<=up2)
+    {
+        temp[k++]=a[j++];
+    }
+    
+}
+
+int copy(int a[], int temp[], int low, int up)
+{
+   for(int i=low; i<=up; i++)
+   {
+       a[i]=temp[i];
+   } 
+}
